c370d0f086597061490a8423428cf6bf383a83e3
Leonid Stepanchenko <leovamhellsing@gmail.com>
2019-05-20 04:02:17 +0300

    lession 2

--- js/bgmenu.js
+++ js/bgmenu.js
@@ -25,3 +25,23 @@ $(document).ready( function(){
 
     })
 })
+
+$(document).ready(function () {
+
+    var items = $('.team__title'); 
+    var $allParrents = $('.team__item'); 
+  
+    items.click(function (e) { 
+      e.preventDefault(); 
+      var $this = $(this); 
+      var $parrent = $this.closest('.team__item');
+  
+      if ($parrent.hasClass('team__item_active')){ 
+        $parrent.removeClass('team__item_active'); 
+      } else {
+        $allParrents.removeClass('team__item_active'); 
+        $parrent.addClass('team__item_active'); 
+      }
+  
+    });
+  });
